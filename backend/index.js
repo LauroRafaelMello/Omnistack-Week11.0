be45ca4267c6b03ae1e178003d798e4c1b21e216
LauroRafaelMello <laurorafa97@gmail.com>
2020-04-02 12:56:01 -0300

    Nodemon integration in the backend

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+/*
+* Métodos HTTP:
+* GET: Buscar uma informação no backend
+* POST: Criar uma informação no backend
+* PUT: Alterar uma informação no backend
+* DELETE: Deletar uma informação no backend
+*/
+
+/*
+* Tipos de parâmetros:
+*
+* Query Params: Parâmetros nomeados na rota após "?" (filtros, paginação)
+* Route Params: Parâmetros utilizados para identicar recursos
+* Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+*/
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+    console.log(body);
+
     return response.json({
         evento: 'Omnistack Week 11.0',
-        aluno: 'Lauro Rafael Mello'
+        aluno: 'Lauro Mello'
     })
 });
 
