df90957c9dabadcd6087500555629eee50f3e8d1
LauroRafaelMello <laurorafa97@gmail.com>
2020-04-01 23:31:44 -0300

    Hello World with React

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.send('Hello World!');
+    return response.json({
+        evento: 'Omnistack Week 11.0',
+        aluno: 'Lauro Rafael Mello'
+    })
 });
 
 app.listen(3333);
