8a492590110d03a931e6c714b21c72cc6e000799
LauroRafaelMello <laurorafa97@gmail.com>
2020-04-07 22:11:19 -0300

    Add resource cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+//Exemplo para fazer o app ser acessado somente de sites específicados
+//app.use(cors( {
+//    origin: 'http://meuapp.com'
+//}));
+//Da forma abaixo da acesso para qualquer site acessar nosso app
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,11 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentsController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionControler = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionControler.create);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
